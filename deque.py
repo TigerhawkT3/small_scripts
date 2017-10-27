17933f73406198d4de6badc604fb53472fc8d8de
David Muller <david@mullerhome.com>
2017-10-27 01:14:44 -0700

    Refactor DQ.__iter__, refactor DQ.__bool__, rewrite Node.__repr__, add maxlen attribute, change count to quantity to allow for count method, write methods for clear, copy, count, extend, extendleft, and index.

--- deque.py
+++ deque.py
@@ -1,6 +1,7 @@
 class DQ:
-    def __init__(self, *items):
-        self.count = 0
+    def __init__(self, *items, maxlen=None):
+        self.maxlen = maxlen
+        self.quantity = 0
         self.first = self.last = None
         for item in items:
             self.append(item)
@@ -11,7 +12,9 @@ class DQ:
             self.last.prior = temp
         else:
             self.first = self.last = Node(item)
-        self.count += 1
+        self.quantity += 1
+        if self.maxlen and self.quantity > self.maxlen:
+            self.popleft()
         return self, item
     def pop(self):
         temp = self.last
@@ -20,7 +23,7 @@ class DQ:
         self.last = self.last.prior
         if self.last:
             self.last.next = None
-        self.count -= 1
+        self.quantity -= 1
         if not self:
             self.first = None
         return self, temp.element
@@ -31,7 +34,9 @@ class DQ:
             self.first.next = temp
         else:
             self.first = self.last = Node(item)
-        self.count += 1
+        self.quantity += 1
+        if self.maxlen and self.quantity > self.maxlen:
+            self.pop()
         return self, item
     def popleft(self):
         temp = self.first
@@ -40,10 +45,29 @@ class DQ:
         self.first = self.first.next
         if self.first:
             self.first.prior = None
-        self.count -= 1
+        self.quantity -= 1
         if not self:
             self.last = None
         return self, temp.element
+    def clear(self):
+        self.last = self.first = None
+        self.quantity = 0
+    def copy(self):
+        return DQ(*self)
+    def count(self, item):
+        return sum(item==element for element in self)
+    def extend(self, other):
+        for item in other:
+            self.append(item)
+    def extendleft(self, other):
+        for item in other:
+            self.appendleft(item)
+    def index(self, item, start=0, stop=0):
+        stop = stop or self.quantity
+        for idx,val in enumerate(self):
+            if val==item and start<=idx<stop:
+                return idx
+        raise ValueError(f'{repr(item)} is not in deque between index {start} and {stop}')
     def __str__(self):
         return 'DQ({})'.format(', '.join(map(repr, self)))
     def __repr__(self):
@@ -52,25 +76,24 @@ class DQ:
         if not self.first:
             return iter('')
         current = self.first
-        while current.next:
+        while current:
             yield current.element
             current = current.next
-        yield current.element
     def __len__(self):
-        return self.count
+        return self.quantity
     def __bool__(self):
-        return bool(len(self))
+        return bool(self.quantity)
     def __add__(self, other):
         return DQ(*self, *other)
 
 class Node:
-    def __init__(self, element=None):
+    def __init__(self, element=None, *args):
         self.next = self.prior = None
         self.element = element
     def __str__(self):
         return str(self.element)
     def __repr__(self):
-        return 'Node({})'.format(repr(self.element))
+        return f'Node({repr(self.element)}, {self.prior and "..."}, {self.next and "..."})'
 
 if __name__ == '__main__':
     d = DQ()
@@ -97,3 +120,16 @@ if __name__ == '__main__':
     print(d.pop())
     print(d.popleft())
     print(d.append(11))
+    print(repr(d.last))
+    print(d.append(12))
+    print(repr(d.last))
+    d.clear()
+    d.extend([1, 2, 3])
+    d.extendleft([3, 4, 5, 6])
+    print(d)
+    e = d.copy()
+    print(e)
+    print(d.count(3), d.count(1), d.count(0))
+    print(d.index(3))
+    print(d.index(3, 4))
+    print(d.index(0))
