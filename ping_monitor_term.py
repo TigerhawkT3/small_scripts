1c7d0dae462be09bad786be4653459bfbd533d4b
David Muller <david@mullerhome.com>
2018-09-29 02:32:01 -0700

    Update ping_monitor_term.py to use the same high-precision colors as the tkinter version uses. Requires a terminal with color support.

--- ping_monitor_term.py
+++ ping_monitor_term.py
@@ -18,15 +18,12 @@ class App:
         self.char = '▓'
         self.limit = 1000
         self.height, self.width = stdscr.getmaxyx()
-        pad = self.pad = curses.newpad(self.height+1, self.width+1)
-        self.pairs = [(curses.COLOR_GREEN, curses.COLOR_GREEN),
-                      (curses.COLOR_YELLOW, curses.COLOR_YELLOW),
-                      (curses.COLOR_MAGENTA, curses.COLOR_MAGENTA),
-                      (curses.COLOR_RED, curses.COLOR_RED)]
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        self.pad = curses.newpad(self.height+1, self.width+1)
+        curses.init_pair(1, 1, 1)
         stdscr.clear()
         data = ping_stream(['ping', '-t', *(sys.argv[1:] or ['www.google.com'])])
-        #data = [f'time={t}ms' for t in (0,0,50,150,250,350,1100,900)]; data.insert(6, 'timed out')
+        # test data:
+        #data = [f'time={t}ms' for t in range(0,1501, 100)]; data.insert(6, 'timed out')
         self.process(data)
     def process(self, data):
         for datum in data:
@@ -35,9 +32,13 @@ class App:
             self.pad.clear()
             if 'time=' in datum:
                 ping = int(datum.partition('time=')[2].partition('ms')[0])
+                half = self.limit/2
+                green,red = 1000,1000-int(1000*abs(min(ping, self.limit) - half)/half)
+                if ping > self.limit/2:
+                    green,red = red,green
+                curses.init_color(1, red, green, 0) # color #1, up to 1000 of each channel
                 fill = self.char * int(self.width * min(ping, self.limit) / self.limit) + '\n'
                 self.pad.addstr((fill * self.height).strip(), curses.color_pair(1))
-                curses.init_pair(1, *self.pairs[min(ping//100, len(self.pairs)-1)])
                 self.pad.refresh(0,0, 0,0, self.height-1,self.width-1)
             elif 'timed out' in datum:
                 curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -45,7 +46,7 @@ class App:
                 for _ in range(3):
                     curses.flash()
                     curses.napms(50)
-                curses.init_pair(1, *self.pairs[-1])
+                curses.init_pair(1, 1, 1)
         # with test data:
         #    curses.napms(1500)
         #self.stdscr.timeout(-1)
